3a248ea2039250a5cb1b23a3d13722dc5dd65611
Marwa <marwahakmi@gmail.com>
2022-08-11 16:53:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const writeCards = list => {
+    let newList = []
+    for (let i = 0; i < list.length; i++) {
+        newList.push(`Thank you, ${list[i]}, for the wonderful surprise gift!`)
+    }
+    return newList
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
+////////////////////////////////////////////////////////////////////
+
+const countDown = (counter) => {
+    do {
+        console.log(counter);
+        counter--;
+    } while (counter >= 0);
+}
+console.log(countDown(4))
